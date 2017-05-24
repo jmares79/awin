8ab940feba382e7d39e6a201b810d338d298b26b
Jose Ares <jmares79@hotmail.com>
2017-05-24 20:36:47 -0300

    Created StreamDataService tests with FileReader mock

--- src/StreamDataBundle/Service/StreamDataService.php
+++ src/StreamDataBundle/Service/StreamDataService.php
@@ -12,6 +12,7 @@ use StreamDataBundle\Interfaces\FileReaderInterface;
 class StreamDataService implements StreamDataInterface
 {
     protected $reader;
+    protected $transactions = [];
 
     public function __construct(FileReaderInterface $reader)
     {
@@ -32,9 +33,16 @@ class StreamDataService implements StreamDataInterface
 
     protected function getData($merchantId)
     {
+        // var_dump($this->reader->getFileRow());
+        // die;
+
         while ($row = $this->reader->getFileRow()) {
             $transaction = $this->reader->parseRow($row);
-
+            echo 'INSIDE METHOD -- ';
+            var_dump($row);
+            var_dump($transaction);
+            var_dump($merchantId);
+            echo PHP_EOL;
             if ($transaction[0] == $merchantId) {
                 $this->transactions[] = $transaction;
             }
