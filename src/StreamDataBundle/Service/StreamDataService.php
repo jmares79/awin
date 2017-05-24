c87999cee64031ebdcd13efe893c37ac96036524
Jose Ares <jmares79@hotmail.com>
2017-05-24 16:09:38 -0300

    Added FileReader in order to simplify StreamDataReader

--- src/StreamDataBundle/Service/StreamDataService.php
+++ src/StreamDataBundle/Service/StreamDataService.php
@@ -4,52 +4,36 @@ namespace StreamDataBundle\Service;
 
 use MerchantBundle\Exceptions\FileParsingException;
 use StreamDataBundle\Interfaces\StreamDataInterface;
+use StreamDataBundle\Interfaces\FileReaderInterface;
 
 /**
  *  Class for implementing a stream data fetcher
  */
 class StreamDataService implements StreamDataInterface
 {
-    const READ_MODE = 'r';
+    protected $reader;
 
-    protected $streamDirectory;
-    protected $streamName;
-    protected $streamSource;
-    protected $handler;
-    protected $data;
-
-    public function __construct($streamDirectory, $streamName)
+    public function __construct(FileReaderInterface $reader)
     {
-        $this->streamDirectory = $streamDirectory;
-        $this->streamName = $streamName;
-    }
-
-    public function setStreamDirectory($dir)
-    {
-        $this->setStreamDirectory = $dir;
-    }
-
-    public function setStreamPath($path)
-    {
-        $this->setStreamPath = $path;
+        $this->reader = $reader;
     }
 
     public function fetchData($merchantId)
     {
-        $this->openStream();
+        $this->reader->openStream();
+        $this->reader->parseHeader();
 
-        $this->parseHeader();
         $data = $this->getData($merchantId);
 
-        $this->closeStream();
+        $this->reader->closeStream();
 
         return $data;
     }
 
     protected function getData($merchantId)
     {
-        while ($row = $this->getStreamRow()) {
-            $transaction = $this->parseRow($row);
+        while ($row = $this->reader->getFileRow()) {
+            $transaction = $this->reader->parseRow($row);
 
             if ($transaction[0] == $merchantId) {
                 $this->transactions[] = $transaction;
@@ -57,56 +41,8 @@ class StreamDataService implements StreamDataInterface
         }
 
         return [
-            'header' => $this->header,
+            'header' => $this->reader->getHeader(),
             'transactions' => $this->transactions
         ];
-    }
-
-    /**
-     * Parses and return a row of the stream
-     *
-     * @return An array with the row data
-     */
-    protected function getStreamRow()
-    {
-        return fgetcsv($this->handler);
-    }
-
-    /**
-     * Parses the header of the stream
-     *
-     * @return An array with the header data
-     */
-    protected function parseHeader()
-    {
-        $headerRow = fgetcsv($this->handler);
-        $this->header = $this->parseRow($headerRow);
-    }
-
-    protected function openStream()
-    {
-        if (!$this->handler = fopen($this->getFullPath(), self::READ_MODE)) throw new FileParsingException();
-    }
-
-    protected function closeStream()
-    {
-        fclose($this->handler);
-    }
-
-    protected function getFullPath()
-    {
-        return $this->streamDirectory . $this->streamName;
-    }
-
-    /**
-     * Parses a common row of the stream
-     *
-     * @param mixed $row
-     *
-     * @return An array with the row data
-     */
-    protected function parseRow($row)
-    {
-        return explode(";", str_replace("\"", "", $row[0]));
     }
 }
--- tests/StreamDataBundle/Service/StreamDataServiceTest.php
+++ tests/StreamDataBundle/Service/StreamDataServiceTest.php
@@ -16,25 +16,25 @@ class StreamDataServiceTest extends TestCase
 
     public function setUp()
     {
-        vfsStreamWrapper::register();
-        // define my virtual file system
-        $directory = [
-            self::FILE_DIR => [
-                self::FILE_NAME =>
-                    '"merchant";"date";"value"
-                     2;"01/05/2010";"£50.00"
-                     2;"01/05/2010";"$66.10"
-                     2;"02/05/2010";"€12.00"
-                     2;"02/05/2010";"£6.50"
-                     1;"02/05/2010";"£11.04"
-                     1;"02/05/2010";"€1.00"
-                     1;"03/05/2010";"$23.05"
-                     2;"04/05/2010";"€6.50"'
-            ]
-        ];
+        // vfsStreamWrapper::register();
 
-        $this->vFileSystem = vfsStream::setup('root', 444, $directory);
-        var_dump($this->vFileSystem->url());
+        // $directory = [
+        //     self::FILE_DIR => [
+        //         self::FILE_NAME =>
+        //             '"merchant";"date";"value"
+        //              2;"01/05/2010";"£50.00"
+        //              2;"01/05/2010";"$66.10"
+        //              2;"02/05/2010";"€12.00"
+        //              2;"02/05/2010";"£6.50"
+        //              1;"02/05/2010";"£11.04"
+        //              1;"02/05/2010";"€1.00"
+        //              1;"03/05/2010";"$23.05"
+        //              2;"04/05/2010";"€6.50"'
+        //     ]
+        // ];
+
+        // $this->vFileSystem = vfsStream::setup('root', 444, $directory);
+        // var_dump($this->vFileSystem->url());
 
         $this->stream = new StreamDataService(self::FILE_DIR, self::FILE_NAME);
     }
