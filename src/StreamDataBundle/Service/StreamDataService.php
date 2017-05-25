71a3d159fafd4f6bb7a0737c66712891de89ea2c
Jose Ares <jmares79@hotmail.com>
2017-05-25 15:04:44 -0300

    +) Added Merchant tests

--- src/StreamDataBundle/Service/StreamDataService.php
+++ src/StreamDataBundle/Service/StreamDataService.php
@@ -30,8 +30,8 @@ class StreamDataService implements StreamDataInterface
     public function fetchData($merchantId)
     {
         $this->reader->openStream();
-        $this->reader->parseHeader();
 
+        $this->reader->parseHeader();
         $data = $this->getData($merchantId);
 
         $this->reader->closeStream();
@@ -49,6 +49,7 @@ class StreamDataService implements StreamDataInterface
     protected function getData($merchantId)
     {
         while ($row = $this->reader->getFileRow()) {
+            var_dump($row);
             $transaction = $this->reader->parseRow($row);
 
             if ($transaction[0] == $merchantId) {
--- tests/FileMerchantTest/Service/FileMerchantServiceTest.php
+++ tests/FileMerchantTest/Service/FileMerchantServiceTest.php
@@ -1,10 +1,44 @@
 <?php
 use PHPUnit\Framework\TestCase;
 
+use MerchantBundle\Service\FileMerchantService;
+use CurrencyBundle\Service\CurrencyConverterService;
+use StreamDataBundle\Service\StreamDataService;
+
 class FileMerchantServiceTest extends TestCase
 {
-    public function testGetData()
-    {
+    protected $mockedConverter;
+    protected $mockedStream;
+    protected $merchant;
 
+    public function setUp()
+    {
+        $this->mockedStream = $this->createMock(StreamDataService::class);
+        $this->mockedConverter = $this->createMock(CurrencyConverterService::class);
+
+        $this->mockedStream
+            ->method('fetchData')
+            ->willReturn(
+                array(
+                    'header' => array("merchant", "date", "value"),
+                    'transactions' => array(
+                        array("2", "01/05/2010", "£50.00"),
+                        array("2", "01/05/2010", "$66.10"),
+                        array("2", "02/05/2010", "€12.00"),
+                        array("2", "02/05/2010", "£6.50"),
+                        array("2", "04/05/2010", "€6.50"),
+                    )
+                )
+            );
+
+        $this->merchant = new FileMerchantService($this->mockedConverter, $this->mockedStream);
+    }
+
+    public function testFetchTransactions()
+    {
+        $this->merchant->fetchTransactions('2');
+        $transactions = $this->merchant->getFetchedTransactions();
+
+        $this->assertCount(5, $transactions);
     }
 }
