7a647883392df971fdd7661d40bc12bbb73374e2
Jose Ares <jmares79@hotmail.com>
2017-05-25 12:57:47 -0300

    Removed debug code

--- src/StreamDataBundle/Service/StreamDataService.php
+++ src/StreamDataBundle/Service/StreamDataService.php
@@ -19,6 +19,14 @@ class StreamDataService implements StreamDataInterface
         $this->reader = $reader;
     }
 
+    /**
+     * Fetchs transactions from a stream
+     *
+     * @param int $merchantId
+     * @param internal $reader Service
+     *
+     * @return An array with all the data to be shown
+     */
     public function fetchData($merchantId)
     {
         $this->reader->openStream();
@@ -31,18 +39,18 @@ class StreamDataService implements StreamDataInterface
         return $data;
     }
 
+    /**
+     * Get transaction information from a file row
+     *
+     * @param int $merchantId
+     *
+     * @return An array with the header and transactions
+     */
     protected function getData($merchantId)
     {
-        // var_dump($this->reader->getFileRow());
-        // die;
-
         while ($row = $this->reader->getFileRow()) {
             $transaction = $this->reader->parseRow($row);
-            echo 'INSIDE METHOD -- ';
-            var_dump($row);
-            var_dump($transaction);
-            var_dump($merchantId);
-            echo PHP_EOL;
+
             if ($transaction[0] == $merchantId) {
                 $this->transactions[] = $transaction;
             }
